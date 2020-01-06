45765d0ba62ffae4346e961a385cfb5f97079527
Tommy Toban <tommy.deppatori@gmail.com>
2020-01-06 16:17:47 +0700

    add http get

--- lib/http.dart
+++ lib/http.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class HttpPage extends StatelessWidget {
+class HttpPage extends StatefulWidget {
+  @override
+  _HttpPageState createState() => _HttpPageState();
+}
+
+class _HttpPageState extends State<HttpPage> {
+
+  String url = "http://txt2html.sourceforge.net/sample.txt";
+  http.Response data;
+  bool load;
+
+  @override
+  void initState() {
+    super.initState();
+    load = true;
+    fetchData();
+  }
+
+  fetchData() async {
+      data = await http.get(url);
+      load = false;
+      setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HTTP"),
+        title: Text("HTTP Page"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+      body:load?CircularProgressIndicator():Container(
+        child: Text(data.body.toString()),
+      )
     );
   }
+
 }
-
-
-
-
-
